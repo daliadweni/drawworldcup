c6f8f0d8aeea70fc96e0080e1500121fe434c33e
unknown <mouhamedaliadoinii@gmail.com>
2022-06-18 18:44:32 +0200

    Fix Bugs

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var groupeA=[{name:"Qatar",img:"./img/QAT.webp"},{name:"Ecuador",img:"./img/ECU.
 
 
 var groupeB=[{name:"England",img:"./img/ENG.webp"},{name:"Iran",img:"./img/IRN.webp"}, 
-{name:"Netherlands",img:"./img/NED.webp"},{name: "Senegal",img:"./img/SEN.avif"}];
+{name:"Netherlands",img:"./img/NED.webp"},{name: "Wales",img:"./img/WAL.webp"}];
 
 
 var groupeC=[{name:"Argentina",img:"./img/ARG.webp"},{name:"Saudi Arabia",img:"./img/KSA.webp"}, 
@@ -37,51 +37,4 @@ for(var draw =0 ; draw <16; draw++){
   hasard.push(a);
 }
 console.log(hasard);}
-function draw(){
 
-    for (var a = 0; a < listeGroupe.length; a++) {
-
-        if (a == 0) {
-            document.write("<div class='row'>");
-        }
-        if (a == 2) {
-            document.write("<div class='row'>");
-        }
-        if (a == 4) {
-            document.write("<div class='row'>");
-        }
-
-        document.write(
-            "<div class='col-6'><table class='table table-bordered border-primary'><thead class='table-primary'><th>N°</th><th>Nom</th> <th>Etat</th> </thead><tbody>"
-            );
-
-            document.write("<h1>groupe "+eval(a + 1)+"</h1>");
-        for (var i = 0; i < listeGroupe[a].length; i++) {
-            var myArray = [0, 1,2,3];    
-            var a =  myArray[Math.floor(Math.random() * myArray.length)];
-            var b =  myArray[Math.floor(Math.random() * myArray.length)];
-
-if(i==a||i==b){
-            document.write("  <tr><th>" + eval(i + 1) + "</th><th><img width='25px' src='" + listeGroupe[a][
-                i
-            ].img + "'> " + listeGroupe[a][i].name + "</th> <th> Qualifie</th>    </tr>");
-        }else {
-    document.write("  <tr><th>" + eval(i + 1) + "</th><th><img width='25px' src='" + listeGroupe[a][
-        i
-    ].img + "'> " + listeGroupe[a][i].name + "</th> <th> Qualifie</th>    </tr>");
-}
-        }
-
-        document.write(" <tbody></table></div>")
-        if ((a == 0)) {
-            document.write("</div>");
-        }
-        if ((a == 2)) {
-            document.write("</div>");
-        }
-        if ((a == 4)) {
-            document.write("</div>");
-        }
-    }
-
-}
